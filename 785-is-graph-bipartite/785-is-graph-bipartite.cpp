70291e1d9ad7a9929fa998454909255304fb68c6
Chandan Kumar <64626028+Chandankr-kr@users.noreply.github.com>
2022-06-24 16:05:22 +0530

    Time: 30 ms (70.22%), Space: 13.7 MB (38.37%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool findSol(vector<vector<int>>& graph,int start,vector<bool>& color,vector<bool>& visited){
+    
+        queue<int> q1;
+        q1.push(start);
+        while(q1.size()){
+            int n=q1.size();
+            for(int i=0;i<n;i++){
+                int front=q1.front();
+                q1.pop();
+                visited[front]=true;
+                for(auto &it:graph[front]){
+                    if(visited[it]==false){
+                        visited[it]=true;
+                        color[it]=!color[front];
+                        q1.push(it);
+                    }
+                    else if(color[it]==color[front])
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<bool> color(n,false);
+        vector<bool> visited(n,false);
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                if(!findSol(graph,i,color,visited))
+                    return false;
+            }
+        }
+        return true;
+    }
+};
