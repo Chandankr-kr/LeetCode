3a10d6cb723413155089358500480de47a440a48
Chandan Kumar <64626028+Chandankr-kr@users.noreply.github.com>
2022-06-25 13:31:12 +0530

    Time: 57 ms (64.87%), Space: 52.1 MB (39.36%) - LeetHub

--- 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
@@ -15,9 +15,10 @@ public:
     vector<TreeNode*> res;
     string findSol(TreeNode* root){
         if(root==NULL)
-            return "";
-        string str="("+findSol(root->left)+to_string(root->val)+findSol(root->right)+")";
+            return "*";
+        string str="("+to_string(root->val)+findSol(root->left)+findSol(root->right)+")";
         mp[str].push_back(root);
+        // cout<<str<<" ";
         return str;
     }
     
