b16d560f5186af8b56f41e348af7e928b6621097
Chandan Kumar <64626028+Chandankr-kr@users.noreply.github.com>
2022-07-05 12:43:48 +0530

    Time: 91 ms (21.83%), Space: 52.2 MB (40.14%) - LeetHub

--- 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
@@ -16,7 +16,7 @@ public:
     string findSol(TreeNode* root){
         if(root==NULL)
             return "*";
-        string str="("+findSol(root->left)+to_string(root->val)+findSol(root->right)+")";
+        string str="("+findSol(root->left)+findSol(root->right)+to_string(root->val)+")";
         mp[str].push_back(root);
         // cout<<str<<" ";
         return str;
