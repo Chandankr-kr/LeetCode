ec35b81a1db27c6814b099f22d4454600b6e28f2
Chandan Kumar <64626028+Chandankr-kr@users.noreply.github.com>
2022-08-06 19:09:10 +0530

    Time: 149 ms (80.99%), Space: 40 MB (66.03%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> adj(n+1);
+    int networkDelayTime(vector<vector<int>>& times, int n, int k){
         vector<int> wt(n+1,1e9+7);
+        vector<vector<pair<int,int>>> adj(n+1);
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq1;
-        wt[k]=0;
-        wt[0]=-1;
-        pq1.push({0,k});
         for(auto &it:times){
             adj[it[0]].push_back({it[1],it[2]});
         }
+        wt[k]=0;
+        wt[0]=0;
+        pq1.push({0,k});
         
         while(pq1.size()){
             auto it=pq1.top();
             pq1.pop();
-            int cost=it.first;
-            int x=it.second;
-            for(auto &it:adj[x]){
-                if(wt[it.first]>it.second+wt[x]){
-                    wt[it.first]=it.second+wt[x];
-                    pq1.push({wt[it.first],it.first});
+            int c=it.first;
+            int v=it.second;
+            for(auto &u:adj[v]){
+                if(c+u.second<wt[u.first]){
+                    wt[u.first]=c+u.second;
+                    pq1.push({c+u.second,u.first});
                 }
             }
         }
-        // for(int i=0;i<wt.size();i++)
-        //     cout<<wt[i]<<" ";
-        
         int res=*max_element(wt.begin(),wt.end());
         return res==1e9+7?-1:res;
     }
