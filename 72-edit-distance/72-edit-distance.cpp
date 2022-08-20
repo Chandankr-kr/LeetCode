78d2c5a98da55d4b118a99180287541bafc40bf6
Chandan Kumar <64626028+Chandankr-kr@users.noreply.github.com>
2022-08-20 17:45:48 +0530

    Time: 215 ms (7.44%), Space: 80.5 MB (12.85%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    int dp[502][502];
-    int findSol(string& str1,int ind1,string &str2,int ind2){
+    int dp[501][501];
+    int findSol(string str1,int ind1,string str2,int ind2){
         if(ind1==str1.size())
             return str2.size()-ind2;
         if(ind2==str2.size())
             return str1.size()-ind1;
         if(dp[ind1][ind2]!=-1)
             return dp[ind1][ind2];
+        
         int res=0;
-        if(str1[ind1]==str2[ind2])
-            res = findSol(str1,ind1+1,str2,ind2+1);
+        if(str1[ind1]==str2[ind2]){
+            res=findSol(str1,ind1+1,str2,ind2+1);
+        }
         else{
-            int insert=findSol(str1,ind1+1,str2,ind2);
-            int remove=findSol(str1,ind1,str2,ind2+1);
-            int replace=findSol(str1,ind1+1,str2,ind2+1);
-            res = 1+min({insert,remove,replace});
+            int r1=findSol(str1,ind1+1,str2,ind2);
+            int r2=findSol(str1,ind1,str2,ind2+1);
+            int r3=findSol(str1,ind1+1,str2,ind2+1);
+            res = 1+min({r1,r2,r3});
         }
         return dp[ind1][ind2]=res;
     }
     
-    int minDistance(string str1, string str2) {
-        if(str1.size()==0)
-            return str2.size();
-        if(str2.size()==0)
-            return str1.size();
+    int minDistance(string word1, string word2) {
+        int m=word1.size();
+        int n=word2.size();
+        if(m==0)
+            return n;
+        if(n==0)
+            return m;
         memset(dp,-1,sizeof dp);
-        return findSol(str1,0,str2,0);
+        return findSol(word1,0,word2,0);
     }
 };
