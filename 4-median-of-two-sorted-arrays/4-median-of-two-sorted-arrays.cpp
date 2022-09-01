d1fe842efa1caf22f515d002ff0e12ee8e2e07a8
Chandan Kumar <64626028+Chandankr-kr@users.noreply.github.com>
2022-09-01 18:18:06 +0530

    Time: 89 ms (13.84%), Space: 89.3 MB (86.92%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    double solve(vector<int>&A,vector<int>&B)
-    {
-        int n1=A.size(),n2=B.size(),total=n1+n2,half=total/2;
-        int left=0,right=n1;
-        while(left<=right)
-        {
-            int idxA=left+(right-left)/2;
-            int idxB=(n1+n2)/2-idxA;
-        
-            //median
-            int leftA=idxA>0?A[idxA-1]:INT_MIN;
-            int rightA=idxA<n1?A[idxA]:INT_MAX;
-            int leftB=idxB>0?B[idxB-1]:INT_MIN;
-            int rightB=idxB<n2 ? B[idxB]:INT_MAX;
-            if(leftA<=rightB && leftB<=rightA)
-            {
-                if(total&1)
-                    return min(rightA,rightB);
-                return (max(leftA,leftB)+min(rightA,rightB))/2.0;
-            }
-            else if(leftA>rightB)
-                right=idxA-1;
-            else
-              left=idxA+1;
-        }
-                               
-        return 0;
-    }
-    
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size()<nums2.size())
-            return solve(nums1,nums2);
-        return solve(nums2,nums1);
         
+        if(nums1.size()>nums2.size())
+            swap(nums1,nums2);
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int lo=0,hi=n1;
+        while(lo<=hi){
+            int c1=(lo+hi)/2;
+            int c2=(n1+n2)/2-c1;
+            
+            int l1=c1==0?INT_MIN:nums1[c1-1];
+            int r1=c1==n1?INT_MAX:nums1[c1];
+            
+            int l2=c2==0?INT_MIN:nums2[c2-1];
+            int r2=c2==n2?INT_MAX:nums2[c2];
+            
+            if(l1>r2)
+                hi=c1-1;
+            else if(r1<l2)
+                lo=c1+1;
+            else{
+                if((n1+n2)%2==0)
+                    return (max(l1,l2)+min(r1,r2))/2.0;
+                else
+                    return min(r1,r2)/1.0;
+            }            
+        }
+        return -1;
     }
 };
