77dc4f53df5e0cc354705300b44fcd15588b80bd
Chandan Kumar <64626028+Chandankr-kr@users.noreply.github.com>
2022-09-13 00:53:46 +0530

    Time: 2175 ms (5.00%), Space: 440.7 MB (5.21%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
+    
     string res="";
-    void findSol(string &str,int left,int right){
+    void findSol(string str,int left,int right){
         if(left<0 or right>=str.size() or str[left]!=str[right])
             return;
-        if(str[left]==str[right]){
-            int k=right-left+1;
-            if(res.size()<k)
-                res=str.substr(left,k);
-        }
+        int k=right-left+1;
+        if(k>res.size())
+            res=str.substr(left,k);
         findSol(str,left-1,right+1);
-        return;
     }
     
     string longestPalindrome(string s) {
-        int  n=s.size();
+        int n=s.size();
+        if(n==1)
+            return s;
         res=s[0];
         for(int i=1;i<n;i++){
             findSol(s,i-1,i);
