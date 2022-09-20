78d1bb8651f9b79ea617c2427eb2126206d67c5d
Chandan Kumar <64626028+Chandankr-kr@users.noreply.github.com>
2022-09-20 21:02:34 +0530

    Time: 575 ms (5.01%), Space: 91.6 MB (18.81%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,56 +1,57 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size(),res=0;
-        stack<int> s1;
+        int n=heights.size();
         vector<int> left,right;
+        stack<int> s1;
+        
         for(int i=n-1;i>=0;i--){
             if(s1.size()==0){
                 right.push_back(n);
-                s1.push(i);
             }
             else{
-                while(s1.size() and heights[i]<=heights[s1.top()]){
+                while(s1.size() and heights[s1.top()]>=heights[i])
                     s1.pop();
-                }
-                if(s1.size()){
-                    right.push_back(s1.top());
-                }
-                else{
+                
+                if(s1.size()==0)
                     right.push_back(n);
-                }
-                s1.push(i);
+                else
+                    right.push_back(s1.top());
             }
+            s1.push(i);
         }
         
+        reverse(right.begin(),right.end());
         while(s1.size())
             s1.pop();
+        
         
         for(int i=0;i<n;i++){
             if(s1.size()==0){
                 left.push_back(-1);
-                s1.push(i);
             }
             else{
-                while(s1.size() and heights[i]<=heights[s1.top()]){
+                while(s1.size() and heights[s1.top()]>=heights[i])
                     s1.pop();
-                }
-                if(s1.size()){
-                    left.push_back(s1.top());
-                }
-                else{
+                
+                if(s1.size()==0){
                     left.push_back(-1);
                 }
-                s1.push(i);
+                else{
+                    left.push_back(s1.top());
+                }
+                
             }
+            s1.push(i);
         }
-        reverse(right.begin(),right.end());
         
+        int res=0;
+        // for(int i=0;i<n;i++){
+        //     cout<<left[i]<<" "<<right[i]<<endl;
+        // }
         for(int i=0;i<n;i++){
-            int temp=(right[i]-left[i]-1)*heights[i];
-            res=max(res,temp);
+            res=max(res,(right[i]-left[i]-1)*heights[i]);
         }
-        
         return res;
     }
 };
