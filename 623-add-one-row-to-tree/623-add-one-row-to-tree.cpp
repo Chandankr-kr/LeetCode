451ba9b3dfa527894cd4ac569ef2beb2717a10ee
Chandan Kumar <64626028+Chandankr-kr@users.noreply.github.com>
2022-10-05 12:38:08 +0530

    Time: 22 ms (87.63%), Space: 25 MB (62.88%) - LeetHub

---
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth==1){
+            TreeNode* res=new TreeNode(val);
+            res->left=root;
+            return res;
+        }
+        
+        int cnt=0;
+        queue<TreeNode*> q1;
+        q1.push(root);
+        
+        while(q1.size()){
+            int n=q1.size();
+            cnt++;
+            for(int i=0;i<n;i++){
+                auto it=q1.front();
+                q1.pop();
+                if(cnt!=depth-1){
+                    if(it->left)
+                        q1.push(it->left);
+                    
+                    if(it->right)
+                        q1.push(it->right);
+                }
+                else{
+                    TreeNode* temp=new TreeNode(val);
+                    
+                        temp->left=it->left;
+                        it->left=temp;
+                    
+                    
+                    TreeNode* temp1=new TreeNode(val);
+                
+                        temp1->right=it->right;
+                        it->right=temp1;
+                    
+                }
+            }
+        }
+        return root;
+    }
+};
