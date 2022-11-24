4222b102269c1eec3e72f2c9d0e0436a14b087c4
Chandan Kumar <64626028+Chandankr-kr@users.noreply.github.com>
2022-11-24 14:58:21 +0530

    Time: 50 ms (44.59%), Space: 13.3 MB (12.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,3 +1,32 @@
+// class Solution {
+// public:
+//     int findSol(string str1,string str2,int i,int j,vector<vector<int>> &res){
+//         if(i==str1.size() or j==str2.size()){
+//             return 0;
+//         }
+        
+//         if(res[i][j]!=-1)
+//             return res[i][j];
+        
+//         int result=0;
+//         if(str1[i]==str2[j]){
+//             result=1+findSol(str1,str2,i+1,j+1,res);
+//         }
+//         else{
+//             result=max(findSol(str1,str2,i+1,j,res),findSol(str1,str2,i,j+1,res));
+//         }
+        
+//         return res[i][j]=result;
+//     }
+    
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int m=text1.size();
+//         int n=text2.size();
+//         vector<vector<int>> res(m+1,vector<int>(n+1,-1));
+//         return findSol(text1,text2,0,0,res);
+//     }
+// };
+
 class Solution {
 public:
     int findSol(string &str1, string &str2,int m,int n,vector<vector<int>> &res){
