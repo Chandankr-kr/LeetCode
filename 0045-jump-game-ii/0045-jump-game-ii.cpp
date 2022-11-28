7c9bad9ff599acbfdc569021a2651179684a9932
Chandan Kumar <64626028+Chandankr-kr@users.noreply.github.com>
2022-11-28 15:46:35 +0530

    Time: 39 ms (64.32%), Space: 16.5 MB (82.30%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int start=0,end=0,cnt=0;
         int n=nums.size();
-        
+        int start=0,end=0,cnt=0;
         while(end<n-1){
-            int maxEnd=end+1;
+            int maxEnd=start+1;
             for(int i=start;i<=end;i++){
                 maxEnd=max(maxEnd,i+nums[i]);
             }
